3f9d81d018772328014c995d13ca2122cb369235
Gabi <38123199+GabrielLeonte@users.noreply.github.com>
2020-03-16 17:59:20 +0200

    Update details.vue
    Crearea unui aspect mult mai curat pentru details.vue

--- src/pages/details.vue
+++ src/pages/details.vue
@@ -4,29 +4,29 @@
       <span>{{ item.service_name }}</span>
       <span class="line"></span>
     </div>
-    <div class="columns is-multiline is-gapless list-container">
-      <div class="column">
-        <span class="status">Status:</span>
-        <span class="data">&#8226; {{ item.status }}</span>
+    <div class="column is-full full">
+      <div class="details">
+        <span class="big-title">Detalii</span> <br />
+        <span>Status {{ item.status }}</span> <br />
+        <span>ID Comanda {{ item.id }}</span> <br />
+        <span>Buget {{ item.budget }} RON </span> <br />
+        <span>Solicitant: {{ item.contact_firstname }} {{ item.contact_lastname }}</span> <br />
+        <span>Contact Solicitant: +{{ item.contact_phone }} (RO)</span> <br />
       </div>
-      <div class="column">
-        <span class="status">ID Cerere:</span>
-        <span class="data">&#8226; {{ item.id }}</span>
+      <div class="details">
+        <span class="big-title">Locatie</span> <br />
+        <span>{{ item.street }}, Numarul {{ item.address_house_number }} </span> <br />
+        <span>Blocul {{ item.address_block_number.toUpperCase() }}</span> <br />
+        <span>Apartamentul {{ item.address_apartament_number }} </span><br />
+        <span v-if="item.taken_by">{{ item }}</span>
       </div>
-      <div class="column">
-        <span class="status">Descriere:</span>
-        <span class="data">&#8226; {{ item.description }}</span>
+      <div class="details">
+        <span class="big-title">Descriere</span> <br />
+        <span>
+          {{ item.description }}
+        </span>
       </div>
-      <div class="column">
-        <span class="status">Buget: </span>
-        <span class="data">&#8226; {{ item.budget }} RON</span>
-      </div>
-      <div class="column">
-        <span class="status">Data </span>
-        <span class="data">&#8226; {{ new Date(item.timestamp).toUTCString() }}</span>
-      </div>
-
-      <div class="column">
+      <!-- <div class="column">
         <div v-if="item.taken_by">
           <span>Curier: {{ item.taken_by }}</span>
           <span class="data"> {{ item.last_location }}</span>
@@ -34,7 +34,7 @@
         <div v-else>
           <span>Nu se pot afisa date de tip geolocatie</span>
         </div>
-      </div>
+      </div>-->
     </div>
     <div class="svg">
       <img src="../assets/svg/details.svg" />
@@ -76,13 +76,12 @@ export default {
   right: 1vh;
 }
 .list-container {
-  margin-left: 20px;
   line-height: 24px;
   margin-top: 20px;
 }
 .status {
   position: relative;
-  color: #585858;
+  color: #393939;
   padding-top: 12px;
   font-weight: 600;
   font-size: 15px;
@@ -92,5 +91,25 @@ export default {
   font-size: 13px;
   color: #393939;
   margin-left: 12px;
+}
+.column {
+  text-align: center;
+}
+.full {
+  display: block;
+  font-size: 13px;
+}
+.details {
+  display: block;
+  margin-top: 20px;
+  box-shadow: 0px 1px 9px 0px rgba(0, 0, 0, 0.18);
+  text-align: left;
+  padding: 10px 10px 5px 10px;
+  border-radius: 2px;
+}
+.big-title {
+  display: block;
+  font-weight: 700;
+  margin-bottom: -15px;
 }
 </style>
